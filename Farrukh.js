9f18434a9f4307ea065e471539a67262b609d517
FarrukhAzeem97 <50796429+FarrukhAzeem97@users.noreply.github.com>
2020-04-09 00:40:15 +0500

    Add files via upload

---
+++ Farrukh.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+//import ReactIntense from 'react-intense';
+import blockimage from './blockchain.jpg';
+class Farrukh extends Component {
+render() {
+return (
+<div>
+<h1>Farrukh Azeem</h1>
+<img src={blockimage} />
+</div>
+); }
+}
+export default Farrukh;
